08473b65192bd50d3d6f0fac27fc40d5c4ddc347
kate-hildebrand <kate.hildebrand@kellogg.northwestern.edu>
2022-05-13 18:33:08 +0000

    Didn't run but was written

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   resources :transactions
+
+  # create a new resource with the rails generate controller ___ command 
+  # then go to app, view, ____ to create an index.html.erb 
 end
